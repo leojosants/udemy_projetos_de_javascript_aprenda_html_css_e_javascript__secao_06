5f1e114eef19483f53c951a9eb06cb49df0701e8
Leonardo Santos <leonardojosantos@gmail.com>
2023-08-25 01:12:50 -0300

    "class 23 - features with javascript"

--- barra_de_progresso/assets/js/app.js
+++ barra_de_progresso/assets/js/app.js
@@ -1,6 +1,35 @@
-// .container
-// .progress_bar
-// .progress
-// .controls
-// #previous_btn
-// #next_btn
+/* selected elements */
+const elementProgressBar = document.querySelector('.progress');
+const elementPreviousBtn = document.querySelector('#previous_btn');
+const elementNextBtn = document.querySelector('#next_btn');
+
+let progress = 0;
+
+/* functions */
+function updateProgressBar() {
+    elementProgressBar.style.width = progress + '%';
+};
+
+function nextStep() {
+    progress += 10;
+
+    if (progress > 100) {
+        progress = 100;
+    }
+
+    updateProgressBar();
+};
+
+function previousStep() {
+    progress -= 10;
+
+    if (progress < 0) {
+        progress = 0;
+    }
+
+    updateProgressBar();
+};
+
+/* events */
+elementNextBtn.addEventListener('click', nextStep);
+elementPreviousBtn.addEventListener('click', previousStep);
